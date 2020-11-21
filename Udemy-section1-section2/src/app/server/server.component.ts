b8bfc123094646db02eafe012e3394441ce6322c
Casey <casey.gardonio@foat.org>
2020-11-21 17:00:20 -0600

    Set-up completed through lecture 2-16

---
+++ Udemy-section1-section2/src/app/server/server.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+    //imports the Component functionality from @angular/core so we can use it as a decorator
+
+@Component({
+    selector: 'app-server',
+        //name of the HTML tag that allows us to reference this component elsewhere
+    templateUrl: './server.component.html'
+        //points to corresponding html file for this component
+})
