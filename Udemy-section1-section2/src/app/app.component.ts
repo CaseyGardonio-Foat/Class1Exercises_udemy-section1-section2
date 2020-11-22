3dc38e7a1176cf4aee45aacb75a9250b7cd4778e
Casey <casey.gardonio@foat.org>
2020-11-21 19:09:57 -0600

    Setup completed through lecture 2-22, including Assignment 1

--- Udemy-section1-section2/src/app/app.component.ts
+++ Udemy-section1-section2/src/app/app.component.ts
@@ -4,6 +4,11 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  // styles: [`
+  //   h3 {color: red}
+  // `]
+  // //alternative to using an array of styleUrls to call different .css files
+
 })
 export class AppComponent {
 }
--- Udemy-section1-section2/src/app/app.module.ts
+++ Udemy-section1-section2/src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { AlertWarningComponent } from './alert-warning/alert-warning.component';
+import { AlertSuccessComponent } from './alert-success/alert-success.component';
   //include the path for any new components added to the declaration below
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    AlertWarningComponent,
+    AlertSuccessComponent
       //register new components here so Angular knows to look for them; include the path in an import statement above
   ],
   imports: [
